171a64b5744c4e69c0964c1baca945fed05a61e0
Senfire <eric.mcghee@northwestern.edu>
2022-11-12 21:35:36 -0600

    Fixed the badge issue, didn't know the syntax changed

--- assets/script.js
+++ assets/script.js
@@ -9,46 +9,30 @@ const TempDiv = document.getElementById("tempurature");
 const HumiDiv = document.getElementById("humidity");
 const WindDiv = document.getElementById("windSpeed");
 const UVIndexC = document.getElementById("UV-index");
-
 const historyData = document.getElementById("history");
 var fivedayHead = document.getElementById("fiveday-header");
 var currentDayHead = document.getElementById("weather-today");
 var calendarDate = document.getElementById("todaysDate");
 let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 
-// const forecastEl = document.querySelectorAll('forecast')
-
 const APIkey = "0becbeca9713066f0639d10be1ed37e3";
 
-
-
-// requestUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=41&lon=87&appid=0becbeca9713066f0639d10be1ed37e3&units=imperial';
-
-//Get request from the weather API
+//Get weather from the API call
 function getWeather(cityName) {
-    // Execute a current weather get request from open weather api
+    //Request URL with the passed variables
     let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIkey;
-    axios.get(queryURL)
-        .then(function (response) {
-
-    // fetch(queryURL)
-    //     .then(function (response) {
-          console.log(response);
-          console.log(response.data.name)
-          console.log(response.data.name)
-          currentDayHead.classList.remove("d-none");
+    axios.get(queryURL) // axios get all to the API
+        .then(function (response) { //handling the response 
+          currentDayHead.classList.remove("d-none"); //display toggle
 
             // Parse response to display current weather
             const currentDate = new Date(response.data.dt * 1000);
-            console.log(currentDate)
             const day = currentDate.getDate();
             const month = currentDate.getMonth() + 1;
             const year = currentDate.getFullYear();
             cityNm.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
             let weatherPic = response.data.weather[0].icon;
-            console.log(weatherPic)
             currentPicEl.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
-            console.log(currentPicEl)
             currentPicEl.setAttribute("alt", response.data.weather[0].description);
             TempDiv.innerHTML = "Temperature: " + k2f(response.data.main.temp) + " &#176F";
             HumiDiv.innerHTML = "Humidity: " + response.data.main.humidity + "%";
@@ -56,46 +40,40 @@ function getWeather(cityName) {
             
             // Get UV Index
             let lat = response.data.coord.lat;
-            console.log(lat)
             let lon = response.data.coord.lon;
-            console.log(lon)
             let UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIkey + "&cnt=1";
-            console.log(UVQueryURL)
             axios.get(UVQueryURL)
-                
                 .then(function (response) {
-                    console.log(response)
                     let UVIndex = document.createElement("span");
                     
                     // When UV Index is good, shows green, when ok shows yellow, when bad shows red
                     if (response.data[0].value < 4 ) {
-                        UVIndex.setAttribute("class", "badge badge-success");
+                        UVIndex.setAttribute("class", "badge bg-success");
                     }
                     else if (response.data[0].value < 8) {
-                        UVIndex.setAttribute("class", "badge warning");
+                        UVIndex.setAttribute("class", "badge bg-warning");
                     }
                     else {
-                        UVIndex.setAttribute("class", "badge danger");
+                        UVIndex.setAttribute("class", "badge bg-danger");
                     }
-                    UVIndex = response.data[0].value;
+                    UVIndex.innerText = response.data[0].value;
                     UVIndexC.innerHTML = "UV Index: ";
                     UVIndexC.append(UVIndex);
-                    UVIndex.innerHTML = response.data[0].value;
-                    UVIndexC.innerHTML = "UV Index: ";
-                    UVIndexC.append(UVIndex);
+                    console.log(UVIndexC)
+                    // UVIndex.innerHTML = response.data[0].value;
+                    // UVIndexC.innerHTML = "UV Index: ";
+                    // UVIndexC.append(UVIndex);
                 });
             
             // Get 5 day forecast for this city
             let cityID = response.data.id;
-            console.log(cityID)
-            let forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&appid=" + APIkey;
+            let forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&appid=" + APIkey; //Request API URL with the city ID
             axios.get(forecastQueryURL)
                 .then(function (response) {
-                    fivedayHead.classList.remove("d-none");
-                    
+                    fivedayHead.classList.remove("d-none"); //toggle the five day display
+                    console.log(response)
                     //  Parse response to display forecast for next 5 days
                     const forecastEl = document.querySelectorAll(".forecast");
-                    console.log(forecastEl)
                     for (let i = 0; i < forecastEl.length; i++) {
                         forecastEl[i].innerHTML = "";
                         const forecastIndex = i * 8 + 4;
@@ -107,9 +85,8 @@ function getWeather(cityName) {
                         forecastDateEl.setAttribute("class", "mt-3 mb-0 forecast-date");
                         forecastDateEl.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
                         forecastEl[i].append(forecastDateEl);
-                        console.log(forecastEl[i])
 
-                        // Icon for current weather
+                        // Card for current weather - append to the page (fiveday)
                         const forecastWeatherEl = document.createElement("img");
                         forecastWeatherEl.setAttribute("src", "https://openweathermap.org/img/wn/" + response.data.list[forecastIndex].weather[0].icon + "@2x.png");
                         forecastWeatherEl.setAttribute("alt", response.data.list[forecastIndex].weather[0].description);
@@ -117,6 +94,9 @@ function getWeather(cityName) {
                         const forecastTempEl = document.createElement("p");
                         forecastTempEl.innerHTML = "Temp: " + k2f(response.data.list[forecastIndex].main.temp) + " &#176F";
                         forecastEl[i].append(forecastTempEl);
+                        const forecastWindEl = document.createElement("p");
+                        forecastWindEl.innerHTML = "Wind: " + response.data.list[forecastIndex].wind.speed + "&deg";
+                        forecastEl[i].append(forecastWindEl);
                         const forecastHumidityEl = document.createElement("p");
                         forecastHumidityEl.innerHTML = "Humidity: " + response.data.list[forecastIndex].main.humidity + "%";
                         forecastEl[i].append(forecastHumidityEl);
@@ -125,7 +105,8 @@ function getWeather(cityName) {
         });
 }
 
-    // Get history from local storage if any
+    // Get history from local storage if any - push search term to Array
+    // Invoke render search history
     searchBtn.addEventListener("click", function () {
       const searchTerm = cityChoice.value;
       getWeather(searchTerm);
@@ -134,24 +115,24 @@ function getWeather(cityName) {
       renderSearchHistory();
   })
 
-  // Clear History button
+  // Clear History button - Clear local storage method, the search history array and reload search history
   clearBtn.addEventListener("click", function () {
       localStorage.clear();
       searchHistory = [];
       renderSearchHistory();
   })
 
-  function k2f(K) {
+  function k2f(K) { // conversion function from kelvin to farenheit
       return Math.floor((K - 273.15) * 1.8 + 32);
   }
 
-  function renderSearchHistory() {
+  function renderSearchHistory() { //rendering the history to the page, styling is handled here as well
       historyData.innerHTML = "";
       for (let i = 0; i < searchHistory.length; i++) {
           const historyItem = document.createElement("input");
           historyItem.setAttribute("type", "text");
           historyItem.setAttribute("readonly", true);
-          historyItem.setAttribute("class", "form-control d-block bg-white");
+          historyItem.setAttribute("class", "form-control d-block bg-light history");
           historyItem.setAttribute("value", searchHistory[i]);
           historyItem.addEventListener("click", function () {
               getWeather(historyItem.value);
